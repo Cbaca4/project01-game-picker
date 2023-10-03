422eb51e1e37beec89900bea92ebd1f16c279c3b
wileland <wileland7@gmail.com>
2023-10-02 21:23:08 -0500

    Will's generic dropdown tmrw add fetch data(stats)

--- script.js
+++ script.js
@@ -6,4 +6,31 @@ fetch(steam).then(function (response) {
 }).then(function (data) {
 
     console.log(data);
+
+var stat1 = data.stats[0].stat.name;
+console.log(stat1)
+
 });
+// Generic Dropdown
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
+      dropdownContent.style.display = "block";
+    } else {
+      dropdownContent.style.display = "none";
+    }
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('button')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.style.display === "block") {
+          openDropdown.style.display = "none";
+        }
+      }
+    }
+  }
+  
