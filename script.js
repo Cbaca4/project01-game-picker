18ca7c8f30828b9216bec7d887ee7f4f157035dc
Cristian <139021004+Cbaca4@users.noreply.github.com>
2023-10-04 19:52:11 -0500

    code

--- script.js
+++
@@ -1,9 +0,0 @@
-//this is for other project
-var steam = "https://pokeapi.co/api/v2/pokemon/ditto";
-fetch(steam).then(function (response) {
-    console.log(response);
-    return response.json();
-}).then(function (data) {
-
-    console.log(data);
-});
