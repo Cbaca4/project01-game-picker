399bc1961c661f1979dd0b533e0d85f2421427f8
wileland <wileland7@gmail.com>
2023-10-05 20:38:23 -0500

    Performing intial commit to advance flow.

--- script.js
+++ script.js
@@ -1,36 +1,108 @@
-//this is for other project
-var steam = "https://pokeapi.co/api/v2/pokemon/ditto";
-fetch(steam).then(function (response) {
-    console.log(response);
-    return response.json();
-}).then(function (data) {
-
-    console.log(data);
-
-var stat1 = data.stats[0].stat.name;
-console.log(stat1)
-
-});
-// Generic Dropdown
 function toggleDropdown() {
-    var dropdownContent = document.getElementById("dropdownContent");
-    if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
+  var dropdownContent = document.getElementById("dropdownContent");
+  if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
       dropdownContent.style.display = "block";
-    } else {
+  } else {
       dropdownContent.style.display = "none";
-    }
   }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('button')) {
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.button')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.style.display === "block") {
-          openDropdown.style.display = "none";
-        }
+          var openDropdown = dropdowns[i];
+          if (openDropdown.style.display === "block") {
+              openDropdown.style.display = "none";
+          }
       }
-    }
   }
+}
+
+function fetchPokemonStats(pokemonName) {
+  let apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
   
+  fetch(apiUrl).then(response => {
+      if (!response.ok) {
+          throw new Error("Network response was not ok");
+      }
+      return response.json();
+  })
+  .then(data => {
+      // Clear existing stats
+      let tableBody = document.getElementById('statsTable').getElementsByTagName('tbody')[0];
+      tableBody.innerHTML = '';
+
+      // Populate table with new stats
+      data.stats.forEach(stat => {
+          let row = tableBody.insertRow();
+          let nameCell = row.insertCell(0);
+          let valueCell = row.insertCell(1);
+
+          nameCell.textContent = stat.stat.name;
+          valueCell.textContent = stat.base_stat;
+      });
+  })
+  .catch(error => {
+      console.error("There was a problem fetching the Pokémon stats:", error);
+  });
+}
+
+// ABILITIES TO PAY THA Biz-zil-LA-TIES
+
+function fetchPokemonStats(pokemonName) {
+  let apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+  
+  fetch(apiUrl).then(response => {
+      if (!response.ok) {
+          throw new Error("Network response was not ok");
+      }
+      return response.json();
+  })
+  .then(data => {
+      populateStats(data.stats);
+      function populateAbilities(abilities) {
+        let abilitiesList = document.getElementById('abilitiesList');
+        abilitiesList.innerHTML = '';  // Clear existing abilities
+    
+        abilities.forEach(ability => {
+            let li = document.createElement('li');
+            li.textContent = ability.ability.name;
+            abilitiesList.appendChild(li);
+        });
+    }
+    
+  })
+  .catch(error => {
+      console.error("There was a problem fetching the Pokémon stats:", error);
+  });
+}
+
+function populateStats(stats) {
+    // Clear existing stats for the selected Pokémon
+    let tableBody = document.getElementById('statsTable').querySelector('tbody');
+    tableBody.innerHTML = '';
+
+    // Populate table with new stats
+    stats.forEach(stat => {
+        let row = tableBody.insertRow();
+        let nameCell = row.insertCell(0);
+        let valueCell = row.insertCell(1);
+
+        nameCell.textContent = stat.stat.name;
+        valueCell.textContent = stat.base_stat;
+    });
+}
+
+function populateAbilities(abilities) {
+    // Here we'll populate the abilities.
+    // For the sake of this example, I'm going to just log them.
+    // You can modify this to display the abilities in the way you prefer.
+    console.log('Abilities:', abilities.map(ability => ability.ability.name));
+}
+
+<!-- Section to display Pokémon abilities -->
+<h2 class="subtitle">Abilities</h2>
+<ul id="abilitiesList">
+    <!-- Abilities will be populated here -->
+</ul>
